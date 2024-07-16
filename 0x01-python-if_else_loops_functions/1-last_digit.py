f8bc6fa02cf2862a4e2846e9f39406cda5c23d19
FarahLajimi <farah.lajimi1@gmail.com>
2024-07-16 07:52:49 -0700

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-o = number%10 if number>10 else number%-10
+o = number % 10 if number > 10 else number % -10
 if o > 5:
     ou = "and is greater than 5"
 elif o == 0:
